0f32c26a2d7de517404ea7a705ff76962860c35d
nnsaideepakreddy <nnsaideepakreddy@gmail.com>
2025-06-26 17:29:41 -0400

    Initial commit - Spring Boot E-Commerce backend

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -2,13 +2,16 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.ecommerce.repository")
+@EntityScan(basePackages = "com.ecommerce.model")
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Starting E-Commerce App...");
-		SpringApplication.run(EcommerceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Starting E-Commerce App...");
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
 }
