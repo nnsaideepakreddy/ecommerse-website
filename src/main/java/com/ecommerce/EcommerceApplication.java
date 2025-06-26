c8aee9a8158d3087c42fe531756c3dbf3fe6919e
nnsaideepakreddy <nnsaideepakreddy@gmail.com>
2025-06-26 18:27:09 -0400

    Fix: added driver

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,26 +1,11 @@
 package com.ecommerce;
-
-import com.ecommerce.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.ecommerce.repository")
-@EntityScan("com.ecommerce.model")
 public class EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner test(UserRepository userRepository) {
-        return args -> {
-            System.out.println("✅ UserRepository is loaded: " + userRepository.getClass());
-        };
     }
 }
